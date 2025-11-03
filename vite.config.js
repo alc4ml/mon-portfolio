1efc8faf46b14ddc9f2a70335bc0c66816f3fd45
alc4ml <moreladjalla@gmail.com>
2025-11-03 19:29:29 +0100

    prepare deployment to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/mon-porfolio/", // obligatoire pour GitHub Pages
   server: {
     host: true,
     port: 5173,
     strictPort: true,
     allowedHosts: 'all', // facultatif
-    origin: 'https://3f595f43fbcb.ngrok-free.app', // 👈 lien ngrok complet ici
+    origin: 'https://3f595f43fbcb.ngrok-free.app', // pour lien ngrok
   },
 })
