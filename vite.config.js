dadb92798056b4597b2d301dbb0f12f83970d2ac
alc4ml <moreladjalla@gmail.com>
2025-11-03 19:32:28 +0100

    prepare deployment to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/mon-porfolio/", // obligatoire pour GitHub Pages
+  base: "/mon-portfolio/", // obligatoire pour GitHub Pages
   server: {
     host: true,
     port: 5173,
