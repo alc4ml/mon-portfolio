ac812453492288e94e7adfd29fd273551d83405c
alc4ml <moreladjalla@gmail.com>
2025-11-04 14:49:25 +0100

    fix: update base path and add custom domain

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", // obligatoire pour GitHub Pages
+  base: "/", // obligatoire pour GitHub Pages
   server: {
     host: true,
     port: 5173,
